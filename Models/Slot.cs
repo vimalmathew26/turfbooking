71be784be09f92116215dbd02bccb5a3505da71e
Vimal Mathew <vimalmathew26@gmail.com>
2025-06-27 13:16:53 +0530

    UI Touchup.

--- Models/Slot.cs
+++ Models/Slot.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace turfbooking.Models
 {
@@ -8,33 +7,38 @@ namespace turfbooking.Models
     {
         public int Id { get; set; }
 
-        
+        [Required(ErrorMessage = "Ground is required.")]
         public int GroundId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Start time is required.")]
+        [Display(Name = "Start Time")]
         public TimeSpan StartTime { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Booking date is required.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Booking Date")]
         public DateTime BookingDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "End time is required.")]
+        [Display(Name = "End Time")]
         public TimeSpan EndTime { get; set; }
 
+        [Display(Name = "Slot Status")]
         public SlotStatus Status { get; set; } = SlotStatus.Available;
 
         [ValidateNever]
         public Ground Ground { get; set; }
 
         public int? BookingId { get; set; }
+
         [ValidateNever]
         public Booking Booking { get; set; }
 
-       
         public enum SlotStatus
         {
             Booked,
             Available,
             Blocked
         }
-
     }
 }
